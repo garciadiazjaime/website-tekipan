64793c6a1c6431d673426da4692f13f908cd8232
Jaime <jaime@mintitmedia.com>
2014-06-22 11:19:40 -0500

    2n try

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Busca trabajo | Encuentra un nuevo empleo ' });
-});
-
-module.exports = router;
