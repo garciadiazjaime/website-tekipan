725821d4ec7b6467ae6975cd78644cb80cced862
Jaime <jaime@mintitmedia.com>
2014-11-17 18:43:47 -0600

    activar emplegob clase

--- scrapper.js
+++ scrapper.js
@@ -18,8 +18,8 @@ function Scrapper(Oferta) {
 
     this.doOcc = function (callback) {
     	console.log('Scrapper::doOcc');
-    	// urllib.request('https://www.occ.com.mx/Buscar_Empleo/Resultados?loc=MX-BCN&hits=50&page=1&ci=tijuana', {
-    	urllib.request('https://www.occ.com.mx/Buscar_Empleo/Resultados?bdtype=OCCM&tm=1&hits=50&page=1&f=true', {
+    	lista_ofertas = []
+    	urllib.request('https://www.occ.com.mx/Buscar_Empleo/Resultados?loc=MX-BCN&hits=100&page=1&ci=tijuana', {
 			method: 'POST',
 		}, function(err, data, res) {
 			if(!err && res.statusCode == 200){
@@ -47,7 +47,8 @@ function Scrapper(Oferta) {
     }
 
     this.doEmpleoNuevo = function(callback){
-		console.log('Scrapper::doEmpleoNuevo');    	
+		console.log('Scrapper::doEmpleoNuevo');
+		lista_ofertas = []
 		urllib.request('http://www.empleonuevo.com/oportunidades/?ciudad=Tijuana&pagina=1&ordenar=fecha&orden=desc&cantidad=100', {
 			method: 'POST',
 		}, function(err, data, res) {
@@ -81,6 +82,43 @@ function Scrapper(Oferta) {
 		});
     }
 
+    this.doEmpleoGob = function(callback){
+    	console.log('Scrapper::doEmpleoGob');
+    	lista_ofertas = [];
+    	urllib.request('http://app.empleo.gob.mx/STPSEmpleoWebBack/busquedaEspecificaOfertas.do?method=encontrar', {
+			method: 'POST',
+			data:{
+				'locationEntity': '2:Baja California',
+				'locationMunicipalities[0]': '4:Tijuana'
+			}
+		}, function(err, data, res) {
+			if(!err && res.statusCode == 200){
+				var $ = cheerio.load(data);
+				var bits = res.headers['set-cookie'][0].split(';');
+				var cookie = res.headers['set-cookie'][1] + ';' + bits[0] + ';';
+
+				getEmpleoGobOfertas(cookie, 1, function(response, data){
+					if(response){
+						setEmpleoGobDescription(cookie, 0, function(response, data){
+							if(response){
+								saveOfertas(lista_ofertas, callback)
+							}
+							else{
+								callback(false, data);
+							}
+						});
+					}
+					else{
+						callback(false, data);
+					}
+				});
+			}
+			else{
+		    	callback(false, err);
+		    }
+		});
+    }
+
     function saveOfertas(data, callback){
     	if(data.length){
 	    	for(var i=0; i<data.length; i++){
@@ -89,10 +127,10 @@ function Scrapper(Oferta) {
 		        });
 			}
 			console.log(data.length + ' ofertas guardads de ' + data[0].tag);
-			callback(true, data.length)
+			callback(true, data.length);
 		}
 		else{
-			callback(true, 0)	
+			callback(true, 'There is no info to save');
 		}
     }
 
@@ -113,6 +151,39 @@ function Scrapper(Oferta) {
 		return value;
     }
 
+	function cleanUpSpecialChars(str){
+	    // str = str.replace(/[ÀÁÂÃÄÅ]/g,"A");
+	    str = str.replace(/[�]/g,"é");
+	    return str;
+	}
+
+	function sanitizeDescription(value){
+		value = value.replace(/<\/?[^>]+(>|$)/g, "");
+		value = value.replace('Funciones: ', '');
+		return value;
+	}
+
+	function cleanString(data){
+		var response = data.replace(/(\r\n\t|\n|\r|\t)/gm,"");
+		response = cleanUpSpecialChars(response.trim());
+		return response;
+	}
+
+	function getTimeFromString(format, data){
+		var response = '';
+		if(format == 'empleogob'){
+			var bits = data.split('de');
+			response =  bits[2] + '-' + getMonthFromString(bits[1]) + '-' + bits[0];
+		}
+		return response;
+	}
+
+	function getMonthFromString(data){
+		var months = ['', 'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+		var response = months.indexOf(data.trim().toLowerCase());
+		return response;
+	}
+
     function setEmpleoNuevoCompletOferta(lista_ofertas, index, callback){
     	if(index < lista_ofertas.length){
 			urllib.request(lista_ofertas[index].source + lista_ofertas[index].href, {
@@ -130,6 +201,71 @@ function Scrapper(Oferta) {
 			callback();
 		}
     }
+
+    function getEmpleoGobOfertas(cookie, page, callback){
+    	var url = page == 1 ? 'http://app.empleo.gob.mx/STPSEmpleoWebBack/busquedaEspecificaOfertas.do?method=orderByColumn&orderType=desc&columnNumber=5' : 'http://app.empleo.gob.mx/STPSEmpleoWebBack/busquedaEspecificaOfertas.do?method=goToPage&goToPageNumber=' + page;
+    	urllib.request(url, {
+			method: 'POST',
+			headers: {
+				'Cookie': cookie,
+			}
+		}, function(err, data, res) {
+			if(!err && res.statusCode == 200){
+				var $ = cheerio.load(data);
+				var trs = $('tbody tr');
+				for(var i=1; i<trs.length; i++){
+					var tds = $(trs[i]).find('td')
+					if($(tds[0]).find('.titulo').length){
+						var obj = {
+							title: cleanString($(tds[0]).find('.titulo').text()),
+				 			href: $(tds[0]).find('.titulo').attr('href'),
+				 			timestamp: getTimeFromString('empleogob', cleanString($(tds[4]).text())),
+				 			description: '',
+				 			salary: cleanString($(tds[3]).text()),
+				 			company: cleanString($(tds[2]).text()),
+				 			tag: 'empleogob',
+				 			source: 'http://app.empleo.gob.mx/',
+						};
+						lista_ofertas.push(obj);
+					}
+				}
+				if(page < 6){
+					getEmpleoGobOfertas(cookie, page+1, callback)
+				}
+				else{
+					callback(true, '')
+				}
+			}
+			else{
+		    	callback(false, err);
+		    }
+		});
+    }
+
+	function setEmpleoGobDescription(cookie, index, callback){
+		if(index < lista_ofertas.length){
+			urllib.request(lista_ofertas[index]['source']+lista_ofertas[index]['href'], {
+				method: 'POST',
+				headers: {
+					'Cookie': cookie,
+				}
+			}, function(err, data, res) {
+				if(!err && res.statusCode == 200){
+					var $ = cheerio.load(data);
+					var tmp = $('.ficha_relacionada')
+					var ps = $(tmp[1]).find('p');
+					lista_ofertas[index]['description'] = sanitizeDescription($(ps[1]).html());
+					setEmpleoGobDescription(cookie, index+1, callback)
+				}
+				else{
+			    	callback(false, err);
+			    }
+			});
+		}
+		else{
+			callback(true, '');
+		}	
+	}
 }
 
 module.exports.Scrapper = Scrapper;
